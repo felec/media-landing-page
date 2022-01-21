49f1d180bfb636a6e5ca16f6a3ea95529c12a154
felec <felecrear@gmail.com>
2022-01-21 08:41:05 -0800

    change scroll limits in pos hook

--- src/hooks/usePositionOnScroll.tsx
+++ src/hooks/usePositionOnScroll.tsx
@@ -48,7 +48,7 @@ export const usePositionOnScroll = (
 
   useEffect(() => {
     scroll.onChange((scroll) => {
-      if (ref.current && scroll < SCROLL_LIMIT + 0.2) {
+      if (ref.current && scroll < SCROLL_LIMIT * 2) {
         const rect = ref.current.getBoundingClientRect();
 
         setPosition({
